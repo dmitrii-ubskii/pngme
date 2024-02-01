b2b9d7b0f4d925994886e6e6ff5fa981581ea552
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-02-01 21:42:26 +0000

    Chapter 1 : ChunkType : complete

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -1,3 +1,75 @@
+use std::{
+	fmt,
+	str::{self, FromStr},
+};
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ChunkType {
+	bytes: [u8; 4],
+}
+
+impl ChunkType {
+	pub fn bytes(&self) -> [u8; 4] {
+		self.bytes
+	}
+
+	pub fn is_valid(&self) -> bool {
+		self.is_reserved_bit_valid()
+	}
+
+	pub fn is_critical(&self) -> bool {
+		(self.bytes[0] & 0x20) == 0
+	}
+
+	pub fn is_public(&self) -> bool {
+		(self.bytes[1] & 0x20) == 0
+	}
+
+	pub fn is_reserved_bit_valid(&self) -> bool {
+		(self.bytes[2] & 0x20) == 0
+	}
+
+	pub fn is_safe_to_copy(&self) -> bool {
+		(self.bytes[3] & 0x20) != 0
+	}
+}
+
+#[derive(Debug)]
+struct InvalidChunkTypeBytes {
+	bytes: [u8; 4],
+}
+
+impl std::error::Error for InvalidChunkTypeBytes {}
+impl fmt::Display for InvalidChunkTypeBytes {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "Invalid chunk type bytes: {:?}", self.bytes)
+	}
+}
+
+impl TryFrom<[u8; 4]> for ChunkType {
+	type Error = crate::Error;
+	fn try_from(bytes: [u8; 4]) -> Result<Self, Self::Error> {
+		if bytes.iter().all(u8::is_ascii_alphabetic) {
+			Ok(Self { bytes })
+		} else {
+			Err(InvalidChunkTypeBytes { bytes }.into())
+		}
+	}
+}
+
+impl FromStr for ChunkType {
+	type Err = crate::Error;
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		Self::try_from(<[u8; 4]>::try_from(s.as_bytes())?)
+	}
+}
+
+impl fmt::Display for ChunkType {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		f.write_str(str::from_utf8(&self.bytes).map_err(|_| fmt::Error)?)
+	}
+}
+
 #[cfg(test)]
 mod tests {
 	use std::{convert::TryFrom, str::FromStr};
